b57e2a958bc37776dc43bd0ea99cdf00916a8f39
kruchy <kruchy61@gmail.com>
2014-11-15 13:45:17 +0100

    asdfs

--- src/agent/Agent.java
+++ src/agent/Agent.java
@@ -3,7 +3,7 @@ package agent;
 import slope.SlopeCell;
 
 public class Agent {
-	 private enum direction {N,NE,E,SE,S,SW,W,NW};
+	private Enum Direction ;{N,NE,E,SE,S,SW,W,NW};
 	 private int speed;
 	 private int skill;
 	 private SlopeCell[][] path;
