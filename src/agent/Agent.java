3109745d4950e904d7fae934b2d6ec61c16cc906
Kruchy <kruchy61@gmail.com>
2014-11-15 13:20:24 +0100

    omg

---
+++ src/agent/Agent.java
@@ -0,0 +1,14 @@
+package agent;
+
+import slope.SlopeCell;
+
+public class Agent {
+	 private enum direction {N,NE,E,SE,S,SW,W,NW};
+	 private int speed;
+	 private int skill;
+	 private SlopeCell[][] path;
+	 
+	 
+	 
+
+}
