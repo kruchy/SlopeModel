ba2b65203417a1ff605f0b7dbb8265cca5adae00
kruchy <kruchy61@gmail.com>
2014-11-15 15:01:50 +0100

    agent

--- main/Main.java
+++ main/Main.java
@@ -1,13 +1,15 @@
 package main;
 
 import slope.Slope;
-import agent.Agent;
 import agent.Skiier;
+
 
 public class Main {
 	public static void main(String[] args){
+	//
 		Skiier s = new Skiier();
-		s.createSkiier(Slope.getSlopeTable());
+		//s.createSkiier(Slope.getSlopeTable());
+		for (int i = 0; i < 10; i ++) System.out.println(i);
 		
 	}
 }
--- slope/SlopeCell.java
+++ slope/SlopeCell.java
@@ -5,10 +5,12 @@ import agent.Skiier;
 public class SlopeCell {
 	private int posx, posy;
 	Skiier skiier;
+	public SlopeCell(){};
+	/*
 	public SlopeCell(int x, int y){
 		posx = x;
 		posy = y;
-	}
+	}*/
 	public void setCell(int x, int y){
 		posx = x; 
 		posy = y;
